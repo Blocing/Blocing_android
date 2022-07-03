c67babab2013c04f321d6d82e4f60c4f09119c45
taewoo <66770613+bn-tw2020@users.noreply.github.com>
2022-07-03 11:53:34 +0900

    Update ApiService.java

--- app/src/main/java/kr/ssc/front/data/api/ApiService.java
+++ app/src/main/java/kr/ssc/front/data/api/ApiService.java
@@ -17,18 +17,9 @@ import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface ApiService {
-    //    @GET("posts/{post}") - 요청메소드 GET, baseUrl에 연결될 EndPoint 'posts/{post}
-//    반환타입 Call<PostResult> - Call은 응답이 왔을때 Callback으로 불려질 타입
-    //    PostResult - 요청GET에 대한 응답데이터를 받아서 DTO 객체화할 클래스 타입 지정
-//    메소드명 "getPosts" - 자유롭게 설정, 통신에 영향 x
-//    매개변수 '@Path("post") String post' - 매개변수 post가 @Path("post")를 보고 @GET 내부 {post}에 대입
+    
     @GET("/idcard/{holder_id}")
     Call<Student> getStudent(@Path("holder_id") String holder_id);
-//                new RestAPITask().execute
-//                (getResources().getString(R.string.apiaddress)+
-//                getResources().getString(R.string.reIssue)+holder_id);
-//    @POST("/attendance")
-//    Call<Student> addAttendance(@Body PostAttendance postattendance);
 
     @POST("/attendance")
     Call<Integer> addAttendance(@Body AttendanceModel attendanceModel);
